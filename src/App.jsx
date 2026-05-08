7da254572a56a8bf182db37e856e11dce936599f
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-05-08 16:13:49 +0600

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+     <h1 className="text-3xl font-bold text-red-500">
+  Tailwind Working ✅
+</h1>
+<button className="btn btn-primary">
+  DaisyUI Button
+</button>
+    </div>
+  )
+}
