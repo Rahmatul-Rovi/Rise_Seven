eb5f7c9d91305e10eed887a3d4020ccc282ebd0a
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-05-11 19:34:04 +0600

    MainLayout Section Updated

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,15 +1,11 @@
 import { Outlet } from "react-router-dom";
-// Ekhane Navbar ar Footer import korben
-// import Navbar from "../components/Navbar";
 
 const MainLayout = () => {
     return (
         <div className="max-w-7xl mx-auto">
-            {/* <Navbar /> */}
             <div className="min-h-[calc(100vh-200px)]">
                 <Outlet />
             </div>
-            {/* <Footer /> */}
         </div>
     );
 };
